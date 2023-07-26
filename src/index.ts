2cf0e7ab7db383ccc87d42d045123a29fa8b0995
lazza89 <nicola.lazzarin89@gmail.com>
2023-07-26 16:30:49 +0200

    base start APIREST

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Test");
+});
+
+app.get("/invitationLink", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+const PORT = (process.env.PORT || 8080) as number;
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+});
