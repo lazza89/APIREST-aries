12503dc2b86a1b7d19dc85f178bd4b14a726a922
lazza89 <nicola.lazzarin89@gmail.com>
2023-08-03 13:34:40 +0000

    create Schema And Cred Def updated

--- src/IssuerController.ts
+++ src/IssuerController.ts
@@ -8,6 +8,12 @@ import {
 import { json } from "body-parser";
 const path = require("path");
 
+enum SchemaAndCredDefInLedger {
+  NONE, //schema and credential definition not present
+  SCHEMA, //schema present, credential definition not present
+  SCHEMA_AND_CRED_DEF, //schema and credential definition present
+}
+
 export class IssuerController {
   private issuer: Issuer;
 
@@ -67,69 +73,99 @@ export class IssuerController {
   public async createSchema(schema: any) {
     console.log(schema);
 
+    //check if schema and credential definition are already in ledger and return SchemaAndCredDefInLedger enum
+    const isPresent = await this.checkSchemaAndCredDefInLedger(schema);
+
+    switch (isPresent) {
+      case SchemaAndCredDefInLedger.NONE:
+        console.log(
+          "Schema not found... creating schema and credential definition"
+        );
+
+        const schemaState = await this.issuer.registerCustomSchema(schema);
+        console.log("Schema created, Id: " + schemaState.schemaId);
+
+        console.log("Creating credential definition...");
+
+        const credDefinition = await this.issuer.registerCredentialDefinition(
+          schemaState.schemaId
+        );
+        console.log(
+          "Credential definition registered, Id: " +
+            credDefinition.credentialDefinitionId
+        );
+
+        await this.writeOnJSON(schemaState, credDefinition);
+
+        return "schema and credential definition created";
+
+      case SchemaAndCredDefInLedger.SCHEMA_AND_CRED_DEF:
+        console.log("Schema and credential definition already created");
+        return "schema and credential definition already created";
+
+      case SchemaAndCredDefInLedger.SCHEMA:
+        console.log("Schema already created, creating credential definition");
+        const schemaLedger =
+          await this.issuer.agent.modules.anoncreds.getCreatedSchemas({
+            schemaName: `${schema.name}`,
+            schemaVersion: `${schema.version}`,
+          });
+        const credDef = await this.issuer.registerCredentialDefinition(
+          schemaLedger[0].schemaId
+        );
+
+        console.log(schemaLedger[0]);
+        console.log(credDef);
+
+        await this.writeOnJSON(schemaLedger[0], credDef);
+        return "schema and credential definition created";
+
+      default:
+        return "error creating schema and credential definition";
+    }
+  }
+
+  private async checkSchemaAndCredDefInLedger(
+    schema: any
+  ): Promise<SchemaAndCredDefInLedger> {
     const schemaLedger =
       await this.issuer.agent.modules.anoncreds.getCreatedSchemas({
         schemaName: `${schema.name}`,
         schemaVersion: `${schema.version}`,
       });
-
-    if (schemaLedger.length == 1) {
+    if (schemaLedger.length != 0) {
       console.log("Schema already created, founded in ledger \n");
-      console.log(schemaLedger[0]);
-      await this.writeOnSchemaJSON(
-        schemaLedger[0].schema,
-        schemaLedger[0].schemaId
-      );
-      return "Schema already created, founded in ledger, saving in schema.json";
+      console.log("cheching credential definition...");
+      const credDefinition =
+        await this.issuer.agent.modules.anoncreds.getCreatedCredentialDefinitions(
+          { schemaId: schemaLedger[0].schemaId }
+        );
+      if (credDefinition.length != 0) {
+        console.log(
+          "Credential definition already created, founded in ledger \n"
+        );
+        console.log("Schema id: ", schemaLedger[0].schemaId, "\n");
+        console.log(
+          "Credential definition id: ",
+          credDefinition[0].credentialDefinitionId,
+          "\n"
+        );
+
+        return SchemaAndCredDefInLedger.SCHEMA_AND_CRED_DEF;
+      }
+      return SchemaAndCredDefInLedger.SCHEMA;
     }
 
-    if (schemaLedger.length > 1) {
-      console.log(
-        "Schema already created, founded in ledger, but more than one? \n"
-      );
-
-      return "Schema already created, founded in ledger, but more than one?, i don't like it!";
-    }
-
-    console.log("Creating schema...");
-    const schemaState = await this.issuer.registerCustomSchema(schema);
-    await this.writeOnSchemaJSON(schemaState.schema, schemaState.schemaId);
-    console.log("Schema created: " + schemaState.schemaId);
-
-    const credDefinition = await this.issuer.registerCredentialDefinition(
-      schemaState.schemaId
-    );
-
-    await this.writeOnCredentialJSON(
-      credDefinition.credentialDefinitionId,
-      schema
-    );
-
-    //await this.writeOnCredentialJSON(
-    console.log("Credential definition registered");
-
-    return "Schema created: " + schemaState.schemaId;
+    return SchemaAndCredDefInLedger.NONE;
   }
 
-  private async writeOnSchemaJSON(schema: any, schemaId: string) {
+  private async writeOnJSON(schema: any, credentialDefId: any) {
     const jsonFile = readJsonFile(path.resolve(__dirname, "schema.json"));
-    jsonFile.anoncreds[schema.name + "-" + schema.version] = {
-      schemaState: schema,
-      schemaId: schemaId,
+    jsonFile.anoncreds[schema.schema.name + "-" + schema.schema.version] = {
+      schemaState: schema.schema,
+      schemaId: schema.schemaId,
+      credentialDefinitionId: credentialDefId.credentialDefinitionId,
     };
     writeJsonFile(path.resolve(__dirname, "schema.json"), jsonFile);
-  }
-
-  private writeOnCredentialJSON(credentialDefId: string, schema: any) {
-    const jsonFile = readJsonFile(
-      path.resolve(__dirname, "credentialDefinition.json")
-    );
-    jsonFile.anoncreds[schema.name + "-" + schema.version] = {
-      credentialDefinitionId: credentialDefId,
-    };
-    writeJsonFile(
-      path.resolve(__dirname, "credentialDefinition.json"),
-      jsonFile
-    );
   }
 }
